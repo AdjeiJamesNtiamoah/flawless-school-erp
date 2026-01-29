1f9d45ca66acd75efb9102ef4bbbe75e61e842ba
AdjeiJamesNtiamoah <jameskofintiamoahadjei@gmail.com>
2026-01-29 22:33:49 +0000

    Add login and logout functions in auth.js
    Implement login and logout functions for user authentication.

--- core/auth.js
+++ core/auth.js
@@ -1,1 +1,23 @@
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const db = JSON.parse(localStorage.getItem("DB"));
+  const user = db.users.find(
+    u => u.email === email && u.password === password
+  );
+
+  if (!user) {
+    alert("Invalid login details");
+    return;
+  }
+
+  localStorage.setItem("currentUser", JSON.stringify(user));
+  routeUser(user.role);
+}
+
+function logout() {
+  localStorage.removeItem("currentUser");
+  location.href = "../auth/login.html";
+}
 
