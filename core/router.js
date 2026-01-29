f1a834ede43af353914c3993f6bde266d8330a8a
AdjeiJamesNtiamoah <jameskofintiamoahadjei@gmail.com>
2026-01-29 22:34:12 +0000

    Add routing function for user roles

--- core/router.js
+++ core/router.js
@@ -1,1 +1,8 @@
+function routeUser(role) {
+  if (role === "admin") location.href = "../admin/admin-dashboard.html";
+  if (role === "hr") location.href = "../hr/hr-dashboard.html";
+  if (role === "teacher") location.href = "../teacher/teacher-dashboard.html";
+  if (role === "student") location.href = "../student/student-dashboard.html";
+  if (role === "parent") location.href = "../parent/parent-dashboard.html";
+}
 
