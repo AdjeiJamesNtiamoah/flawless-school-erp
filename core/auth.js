7ed13d7af12848892a390cd6d4ec08893547d890
AdjeiJamesNtiamoah <jameskofintiamoahadjei@gmail.com>
2026-01-30 10:05:42 +0000

    Update auth.js

--- core/auth.js
+++ core/auth.js
@@ -4,11 +4,19 @@ function login(email, password) {
       return db.collection("users").where("email", "==", email).get();
     })
     .then(snapshot => {
+      if (snapshot.empty) {
+        alert("No role assigned. Contact admin.");
+        auth.signOut();
+        return;
+      }
+
       const user = snapshot.docs[0].data();
+
       if (user.role === "admin") location.href = "admin/admin-dashboard.html";
-      if (user.role === "teacher") location.href = "teacher/teacher-dashboard.html";
-      if (user.role === "student") location.href = "student/student-dashboard.html";
-      if (user.role === "parent") location.href = "parent/parent-dashboard.html";
+      else if (user.role === "teacher") location.href = "teacher/teacher-dashboard.html";
+      else if (user.role === "student") location.href = "student/student-dashboard.html";
+      else if (user.role === "parent") location.href = "parent/parent-dashboard.html";
+      else alert("Invalid role");
     })
     .catch(err => alert(err.message));
 }
