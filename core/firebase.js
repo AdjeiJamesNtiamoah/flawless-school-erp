b940130607ab4ae5a8b2220ae96b6696729033a8
AdjeiJamesNtiamoah <jameskofintiamoahadjei@gmail.com>
2026-01-30 08:55:05 +0000

    Create firebase.js

---
+++ core/firebase.js
@@ -0,0 +1,23 @@
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"></script>
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCGJpJpQNud5Z7DFqlS9qzFUGS77ytmrsk",
+    authDomain: "flawless-school-erp.firebaseapp.com",
+    projectId: "flawless-school-erp",
+    storageBucket: "flawless-school-erp.firebasestorage.app",
+    messagingSenderId: "975957541925",
+    appId: "1:975957541925:web:3d4a025fb4bf0e7428abcc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
