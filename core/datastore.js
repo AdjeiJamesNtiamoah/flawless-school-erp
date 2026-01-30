b2b74d99182ee89cdd5619c41a3eccccd29e0461
AdjeiJamesNtiamoah <jameskofintiamoahadjei@gmail.com>
2026-01-30 08:08:17 +0000

    Update datastore.js

--- core/datastore.js
+++ core/datastore.js
@@ -7,7 +7,22 @@ const DB = {
       password: "1234",
       role: "admin"
     }
-  ]
+  ],
+
+  classes: [
+    {
+      id: "CLS1",
+      name: "Grade 5A",
+      teacherEmail: "teacher@school.com"
+    }
+  ],
+
+  students: [
+    { id: "ST1", name: "John Doe", classId: "CLS1" },
+    { id: "ST2", name: "Mary Ann", classId: "CLS1" }
+  ],
+
+  attendance: []
 };
 
 if (!localStorage.getItem("DB")) {
